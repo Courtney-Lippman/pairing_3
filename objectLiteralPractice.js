be44894b851d946a9a800a5e802f64732d212bf7
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-05 08:58:57 -0600

    Add object literal and two key value pairs.

---
+++ objectLiteralPractice.js
@@ -0,0 +1,14 @@
+var person = {
+  key1: "a",
+  key2: "b",
+  key3: "c",
+  key4: "d"
+}
+
+console.log(person);
+console.log(Object.keys(person)[0], Object.values(person)[0]);
+
+person.key5 = "e"
+person.key6 = "f"
+
+console.log(person);
